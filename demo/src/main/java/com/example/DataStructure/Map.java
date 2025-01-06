d2d2309f9d2321357b8759597ba9041f1e9f74fe
Nikita Sryvkov <n.sryvkov@yandex.ru>
2025-01-07 00:15:23 +0300

    feat: add custom HashTable and Map

--- demo/src/main/java/com/example/DataStructure/Map.java
+++ demo/src/main/java/com/example/DataStructure/Map.java
@@ -1,5 +1,33 @@
 package com.example.DataStructure;
 
-public class Map {
+public class Map<K, V> {
+    private MyHashTable<K, V> hashTable;
 
+    public Map() {
+        this.hashTable = new MyHashTable<>();
+    }
+
+    public void put(K key, V value) {
+        hashTable.add(key, value);
+    }
+
+    public V get(K key) {
+        return hashTable.remove(key);
+    }
+
+    public V remove(K key) {
+        return hashTable.remove(key);
+    }
+
+    public int size() {
+        return hashTable.size();
+    }
+
+    public boolean contains(K value) {
+        return hashTable.containsKey(value);
+    }
+
+    public Iterable<V> values() {
+        return hashTable.values();
+    }
 }
--- demo/src/main/java/com/example/DataStructure/MyHashTable.java
+++ demo/src/main/java/com/example/DataStructure/MyHashTable.java
@@ -1,5 +1,112 @@
 package com.example.DataStructure;
 
-public class MyHashTable {
+import java.util.Collection;
+import java.util.Iterator;
 
+public class MyHashTable<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+    private MyLinkedList<KeyValue<K, V>>[] table;
+    private int size;
+    private int capacity;
+
+    public MyHashTable() {
+        this.table = new MyLinkedList[DEFAULT_CAPACITY];
+        this.size = 0;
+        this.capacity = DEFAULT_CAPACITY;
+    }
+
+    public void add(K key, V value) {
+        growIfNeeded();
+
+        int bucketNumber = findBucketNumber(key);
+        if (table[bucketNumber] == null) {
+            table[bucketNumber] = new MyLinkedList<>();
+        }
+
+        table[bucketNumber].add(new KeyValue<>(key, value));
+        size++;
+    }
+
+    public V remove(K key) {
+        int bucketNumber = findBucketNumber(key);
+        MyLinkedList<KeyValue<K, V>> requiredBucket = table[bucketNumber];
+
+        Iterator<KeyValue<K, V>> iterator = requiredBucket.iterator();
+        while (iterator.hasNext()) {
+            KeyValue<K, V> keyValue = iterator.next();
+
+            if (keyValue.getKey().equals(key)) {
+                iterator.remove();
+                size--;
+                return keyValue.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    public V get(K key) {
+        int bucketNumber = findBucketNumber(key);
+        MyLinkedList<KeyValue<K, V>> requiredBucket = table[bucketNumber];
+
+        for (KeyValue<K, V> keyValue : requiredBucket) {
+            if (keyValue.getKey().equals(key)) {
+                return keyValue.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Iterable<V> values() {
+        MyLinkedList<V> values = new MyLinkedList<>();
+        for (MyLinkedList<KeyValue<K, V>> list : table) {
+            if (list != null) {
+                for (KeyValue<K, V> keyValue : list) {
+                    values.add(keyValue.getValue());
+                }
+            }
+        }
+
+        return values();
+    }
+
+    private int findBucketNumber(K key) {
+        return key.hashCode() % capacity;
+    }
+
+    private void growIfNeeded() {
+        if (size >= capacity * LOAD_FACTOR) {
+            grow();
+        }
+    }
+
+    private void grow() {
+        capacity = capacity * 2;
+        MyLinkedList<KeyValue<K, V>>[] newTable = new MyLinkedList[capacity];
+
+        for (MyLinkedList<KeyValue<K, V>> list : table) {
+            if (list != null) {
+                for (KeyValue<K, V> keyValue : list) {
+                    int newBucketNumber = findBucketNumber(keyValue.getKey());
+                    if (newTable[newBucketNumber] == null) {
+                        newTable[newBucketNumber] = new MyLinkedList<>();
+                    }
+
+                    newTable[newBucketNumber].add(keyValue);
+                }
+            }
+        }
+
+        table = newTable;
+    }
 }
